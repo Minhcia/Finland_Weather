95c3e7b3a13bdd839f4a83d81a4dc1b99b94024b
Minh <aa4078@student.jamk.fi>
2024-03-07 02:11:44 +0200

    update

---
+++ team3/Controllers/HomeController.cs
@@ -0,0 +1,135 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using team3.Models;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.Linq;
+
+namespace team3.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index(string location = "")
+    {
+        if (string.IsNullOrEmpty(location))
+        {
+
+            return View();
+        }
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                ViewData["Location"] = location;
+
+                //API address variable
+                String URLString = @"https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi%3A%3Aforecast%3A%3Aedited%3A%3Aweather%3A%3Ascandinavia%3A%3Apoint%3A%3Atimevaluepair&place=" + location.Trim().ToLower() + "&fbclid=IwAR0WkICmGBIoxd2uOLMAy5Jeh4UD1nuB1TRyz83oGNuKY6KToeJ_kYRXWyM";
+
+
+                // Call API by http method
+                HttpResponseMessage response = await client.GetAsync(URLString);
+                response.EnsureSuccessStatusCode(); // Throw an exception if the status code is not a success code.
+                string content = await response.Content.ReadAsStringAsync();
+
+                XmlReader xmlReader = XmlReader.Create(new StringReader(content));
+
+
+                // Necessary variables
+                string currTempurature = "mts-1-1-Temperature";
+                string currWindSpeed = "mts-1-1-windspeedms";
+                DateTime now = new DateTime();
+                XElement el = new XElement("root");
+                float temper = 0;
+                float wind = 0;
+
+                // While loop through currently data
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "wml2:MeasurementTimeseries")
+                    {
+                        if (xmlReader.HasAttributes)
+                        {
+                            if (xmlReader.GetAttribute("gml:id") != null)
+                            {
+                                string atrr = xmlReader.GetAttribute("gml:id").ToLower().Trim();
+                                if (atrr == currWindSpeed.ToLower().Trim())
+                                {
+                                    el = (XElement)XNode.ReadFrom(xmlReader);
+                                    IEnumerable<XElement> names =
+                                                (from e in el.Elements()
+                                                 select e).ToList();
+                                    foreach (XElement n in names)
+                                    {
+                                        string a = n.Value;
+                                        a = a.Replace("\n", "").Replace("\t", "").Trim();
+
+                                        now = DateTime.Parse(a.Split(' ')[0]).AddHours(-4);
+                                        int ssnow = now.Hour;
+                                        if (ssnow == DateTime.Now.Hour && now.Date == DateTime.Now.Date)
+                                        {
+                                            wind = float.Parse(a.Split('Z')[1].Trim());
+                                            ViewData["DateNow"] = now;
+                                            ViewData["WindSpeed"] = a.Split('Z')[1].Trim();
+                                        }
+                                    }
+
+                                }
+                                
+                                if (atrr == currTempurature.ToLower().Trim())
+                                {
+                                    el = (XElement)XNode.ReadFrom(xmlReader);
+                                    IEnumerable<XElement> names =
+                                                (from e in el.Elements()
+                                                 select e).ToList();
+                                    foreach (XElement n in names)
+                                    {
+                                        string a = n.Value;
+                                        a = a.Replace("\n", "").Replace("\t", "").Trim();
+
+                                        now = DateTime.Parse(a.Split(' ')[0]).AddHours(-4);
+                                        int ssnow = now.Hour;
+                                        if (ssnow == DateTime.Now.Hour && now.Date == DateTime.Now.Date)
+                                        {
+                                            temper = float.Parse(a.Split('Z')[1].Trim());
+                                            ViewData["DateNow"] = now;
+                                            ViewData["Temperature"] = a.Split('Z')[1].Trim();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Feel like calculation 
+                ViewData["FeelLike"] = 13.2 + 0.6215 * temper - 11.37 * Math.Pow(wind, 0.16) + 0.3965 * temper * Math.Pow(wind, 0.16);
+                xmlReader.Close();
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = "Not Found";
+            }
+        }
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ team3/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+//function myFunction() {
+//    // Get the selected city from the dropdown
+//    const locationDropdown = document.getElementById("location");
+//    const selectedCity = locationDropdown.value;
+
+//    // You can replace the following data with actual API calls to get real weather data.
+//    // For this example, we'll set dummy values.
+//    const dummyWeatherData = {
+//        none: {temper: 0, wind: 0, feel: 0 },
+//        Helsinki: { temper: 20, wind: 7, feel: 15 },
+//        Lahti: { temper: 18, wind: 6, feel: 14 },
+//        Lappeenranta: { temper: 22, wind: 8, feel: 17 },
+//    };
+
+//    // Update the content in the divs with the selected city's weather data
+//    document.getElementById("temper").textContent = dummyWeatherData[selectedCity].temper;
+//    document.getElementById("wind").textContent = dummyWeatherData[selectedCity].wind;
+//    document.getElementById("feel").textContent = dummyWeatherData[selectedCity].feel;
+//}
